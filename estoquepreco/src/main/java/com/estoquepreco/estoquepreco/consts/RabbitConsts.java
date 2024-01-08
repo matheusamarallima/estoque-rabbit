ee2ff6505c6b96f015d1c9c7d06aa24877759fb9
Mathe <matheusamrl@gmail.com>
2024-01-08 18:54:16 -0300

    consts

---
+++ estoquepreco/src/main/java/com/estoquepreco/estoquepreco/consts/RabbitConsts.java
@@ -0,0 +1,6 @@
+package com.estoquepreco.estoquepreco.consts;
+
+public class RabbitConsts {
+    public static final String FILA_ESTOQUE = "Estoque";
+    public static final String FILA_PRECO = "Preco";
+}
